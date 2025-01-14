b2c96ff391553912851e4524a15c69441af786f1
TifTif1 <liraztiferet@gmail.com>
2025-01-14 19:27:31 +0200

    ...

--- ChessProject/ChessProject/Knight.cpp
+++ ChessProject/ChessProject/Knight.cpp
@@ -6,21 +6,26 @@ Knight::Knight(int col, int row, Board& board, char type, int player) :
 	B._board[row][col] = this;
 }
 
+/*
+Role: Moves the Knight to a specified location
+Input: Target column (col) and row (row)
+Output: Updates the Knight's position, or throws an exception if the move is illegal.
+*/
 void Knight::move(int col, int row)
 {
     if (isMoveLegal(col, row))
     {
         int c = getCol();
         int r = getRow();
-        Tool* dest = B._board[row][col];
+        Tool* dest = B._board[row][col]; // Save destination piece
         B._board[row][col] = this;
-        B._board[getRow()][getCol()] = nullptr;
+        B._board[getRow()][getCol()] = nullptr; // Clear previous position
         setCol(col);
         setRow(row);
-        if (B.checkChess())
+        if (B.checkChess()) // Check if move puts King in check
         {
-            B._board[r][c] = this;
-            B._board[row][col] = dest;
+            B._board[r][c] = this; // Revert position
+            B._board[row][col] = dest; // Restore destination piece
             setCol(c);
             setRow(r);
             B._code = 4;
@@ -28,11 +33,11 @@ void Knight::move(int col, int row)
         }
         else if (dest)
         {
-            delete(dest);
+            delete(dest); // Delete the captured piece
         }
         return;
     }
-    throw LocationException();
+    throw LocationException(); // Throw exception if move is illegal
 }
 
 bool Knight::isBlocked(int col, int row) const
@@ -40,12 +45,17 @@ bool Knight::isBlocked(int col, int row) const
     return false;
 }
 
+/*
+ Role: Checks if a move is legal for the Knight
+ Input: Target column (col) and row (row)
+ Output: Returns true if the move is legal, false otherwise, updating the board's error code if invalid.
+*/
 bool Knight::isMoveLegal(int col, int row) const
 {
-    int rowDiff = abs(getRow() - row);
-    int colDiff = abs(getCol() - col);
+    int rowDiff = abs(getRow() - row); // The distance from the current row to the target row
+    int colDiff = abs(getCol() - col); // The distance from the current column to the target column
 
-    if ((rowDiff == 2 && colDiff == 1) || (rowDiff == 1 && colDiff == 2))
+    if ((rowDiff == 2 && colDiff == 1) || (rowDiff == 1 && colDiff == 2)) // check if the move is legal
     {
         return true;
     }
